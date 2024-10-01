b7353bafabac682da7eb4bdaf5604b9aa23ea3dc
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:57 +0530

    Add files via upload

---
+++ StudentDetails.py
@@ -0,0 +1,10 @@
+name=input("enter your name of the student:")
+roll_number=int(input("enter the roll number of the student:"))
+cgpa=float(input("Enter the CGPA of the student:"))
+percentage_marks=cgpa*10
+
+print("Name of the Student:",name)
+print("Roll number of the Student:",roll_number)
+print("CGPA of the student:",cgpa)
+print("Percentage of the student:",percentage_marks,"%")
+
