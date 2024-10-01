15c8f7b2aface92f0ca2014b19b4d6bfca88f6df
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-01 15:22:46 +0530

    Document header added!

--- StudentDetails.py
+++ StudentDetails.py
@@ -1,8 +1,16 @@
+'''AUTHOR:GANESH CHANDRAN'''
+
+
+''' COLLECTING THE DETAILS OF A STUDENT'''
 name=input("enter your name of the student:")
 roll_number=int(input("enter the roll number of the student:"))
 cgpa=float(input("Enter the CGPA of the student:"))
+
+'''CALCULATING PERCENTAGE MARKS WITH A STUDENT'S CGPA'''
 percentage_marks=cgpa*10
 
+
+'''PRINTING ALL THE DETAILS'''
 print("Name of the Student:",name)
 print("Roll number of the Student:",roll_number)
 print("CGPA of the student:",cgpa)
