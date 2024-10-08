e006868a2a74eda14b7574aab70d0f41935bb471
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-08 14:41:00 +0530

    create,concatenate

---
+++ lms task 7.py
@@ -0,0 +1,10 @@
+'''AUTHOR:GANESH
+08/10/2024
+Create, concatenate, and print a string and access a sub-string from a given string.'''
+
+first_name="Ganesh"
+last_name="Chandran"
+name=first_name+" "+last_name
+print(name)
+lstname=name[7:15]
+print(lstname)
