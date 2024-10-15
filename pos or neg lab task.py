3214b02dfe6e6798bc4f01ba38e28af0caf3cad1
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-15 14:02:23 +0530

    checkin the positivty of a number

---
+++ pos or neg lab task.py
@@ -0,0 +1,11 @@
+'''AUTHOR:GANESH
+15/10/2024
+CHECKING WHETHER A NUMBER IS POSITIVE OR NOT OR ZERO'''
+
+number=int(input("Enter a number: "))
+if number>0:
+    print("The given number",number,"is positive.")
+elif number<0:
+    print("The given number",number,"is negative.")
+else:
+    print("The number entered is 0")
