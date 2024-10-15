91e3294be7c804390a030aea7a4525967d82d0c1
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-15 14:41:38 +0530

    Add files via upload

---
+++ avg h of boys nd girls.py
@@ -0,0 +1,22 @@
+'''AUTHOR:GANESH
+15/10/2024
+To find the average height of boys and girl in calss of n students'''
+n=int(input("ener the number of student in a class"))
+btotal=0
+gtotal=0
+tbh=0
+tgh=0
+for i in range(0,n):
+    gender=input("enter your gender M/F")
+    height=float(input("enter your height"))
+    if gender=="M" or gender=="m":
+        btotal+=1
+        tbh=tbh+height
+    else:
+        gtotal+=1
+        tgh=tgh+height
+
+avg_bh=tbh/btotal
+avg_gh=tgh/gtotal
+print("Average height of boys",avg_bh)
+print("Average height of girls",avg_gh)
