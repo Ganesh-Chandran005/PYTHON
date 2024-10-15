cf30b6ac55afa71ceb6e6a44cbdecb4a240ac7ee
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-15 16:05:02 +0530

    Add files via upload

---
+++ odd numbers to n.py
@@ -0,0 +1,9 @@
+'''AUTHOR:GANESH
+15/10/2024
+finding odd nummber in a user given limit'''
+limit=int(input("enter a limit"))
+odd_number=1
+while odd_number<limit:
+    print(odd_number,"\t",end="")
+    odd_number+=2
+
