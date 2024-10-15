54fe70399405ec42764e29b469deb9897899ede8
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-15 14:59:47 +0530

    Add files via upload

---
+++ colour according to code.py
@@ -0,0 +1,12 @@
+'''AUTHOR:GANESH
+15/10/2024
+FINDING THE COLOR ACCORDING TO THE CODE'''
+code=input("enter the code R/G/B: ")
+if code=="R":
+    print("COLOR IS RED")
+elif code=="G":
+    print("COLOR IS GREEN")
+elif code=="B":
+    print("COLOR IS BLUE")
+else:
+    print("WRONG CODE")
