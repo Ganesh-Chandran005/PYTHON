d4d550ea5851f9f65b62a47b4b073c60f19bffe8
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-15 15:39:51 +0530

    Add files via upload

---
+++ driving liscence.py
@@ -0,0 +1,9 @@
+'''AUTHOR:GANESH
+15/10/2024
+To check whether eligible to take driving liscence'''
+age=int(input("enter your age"))
+print("AGE:",age)
+if age>=18:
+    print("You are ELIGIBLE")
+else:
+    print("NOT ELIGIBLE")
