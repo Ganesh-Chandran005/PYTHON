39c5d8b8c50019d8eee0a6c7a748a81e4cf78871
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-15 14:46:53 +0530

    Add files via upload

---
+++ largest of 2.py
@@ -0,0 +1,11 @@
+'''AUTHOR:GANESH
+15/10/2024
+LARGEST OF TWO'''
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+if num1>num2:
+    print(num1,"is greater")
+elif num2>num1:
+    print(num2,"is greater")
+else:
+    print(num1,"and",num2,"are equal")
