bce51b25e4549cdebd86758b0f4036ba21468cfd
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-10-15 15:13:26 +0530

    Add files via upload

---
+++ Simple intrest.py
@@ -0,0 +1,8 @@
+'''AUTHOR:GANESH
+15/10/2024
+To find the simple intrest'''
+pr_amnt=float(input("enter the principle amount"))
+time=int(input("enter the time in months"))
+rate=float(input("enter the rate"))
+SI=pr_amnt*time*rate
+print("The simple interest is",SI)
