bcde73d656753009740b27839c374d6fe80da222
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2024-11-30 10:49:24 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+'''AUTHOR:GANESH
+30/11/24
+'''
+
+def add_2_number(a,b):
+    sum=a+b
+    print("THE sum of the Numbers",sum)
+num1=int(input("enter a number:"))
+num2=int(input("enter a number: "))
+add_2_number(num1,num2)
